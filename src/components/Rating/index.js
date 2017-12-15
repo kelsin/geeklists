4d2c73808155aaf1d853f5aabe45015d85255ebb
Christopher Giroir <kelsin@valefor.com>
2017-12-15 01:05:21 -0800

    Adding bgg links to ratings

--- src/components/Rating/index.js
+++ src/components/Rating/index.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import BGGLink from '../BGGLink';
 
 class Rating extends Component {
   render() {
     let slug = this.props.slug;
-    let { id, rating, objectid, objectname,
+    let { geeklist_id, id, rating, objectid, objectname,
           username, summary } = this.props.rating;
 
     return (
       <tr key={id}>
         <td>{rating}</td>
-        {username && <td><Link to={"/group/" + slug + "/user/" + username}>{username}</Link></td>}
-        {objectid && <td><Link to={"/group/" + slug + "/game/" + objectid}>{objectname}</Link></td>}
+        {username && <td>
+            <Link to={"/group/" + slug + "/user/" + username}>{username}</Link>
+            &nbsp;
+            <BGGLink type="geeklist" id={`${geeklist_id}/item/${id}#item${id}`} />
+        </td>}
+        {objectid && <td>
+            <Link to={"/group/" + slug + "/game/" + objectid}>{objectname}</Link>
+            &nbsp;
+            <BGGLink type="geeklist" id={`${geeklist_id}/item/${id}#item${id}`} />
+        </td>}
         <td>{summary}</td>
       </tr>
     );
